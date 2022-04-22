70c92414b67af0819a8aceec50b76738ce4eb7ef
LevGaidukov <madmanlev@gmail.com>
2022-04-22 20:11:17 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace uyd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(554, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(157, 396);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(272, 396);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(529, 393);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(448, 391);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(635, 393);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(64, 353);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 20);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(157, 353);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(272, 350);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace uyd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int j = 0; j < bl.Length; j++)
+            {
+                //создаем потоковые объекты
+                bl[j] = new Ball(j, j * 10, 10, 10, j + 1, j + 1, Pens.Magenta, true);
+                //подписываемся на событие
+                bl[j].dl += new Ball.DlTp(Invalidate);
+            }
+        }
+        public class Ball
+        {
+            public Pen color;
+            public bool vis;
+            int x, y; // координаты
+            int dx, dy; //приращение координат-определяет скорость
+            int w, h; //ширина высота шарика
+            public bool live; // признак жизни
+            public delegate void DlTp();// Объявление типа (делегат) и
+                                        //создание пока что пустой ссылки для организации в последующем
+                                        // с помощью ее вызова функции Invalidate()для главного потока
+            public DlTp dl;
+            public Thread thr; //Создание ссылки на потоковый объект
+                               // потоковая функция
+            void FnThr()
+            {
+                while (live)
+                { //здесь отражемся от границ области
+                    if (x < 0 || x > 200) dx = -dx;
+                    if (y < 0 || y > 200) dy = -dy;
+                    //здесь пересчитываем координаты
+                    x += dx;
+                    y += dy;
+                    Thread.Sleep(100);//спим
+                    dl(); //вызываем с помощью делегата Invalidate()
+                }
+                w = h = 0; //схлопываем шарик
+                dl(); //вызываем с помощью делегата Invalidate()
+            }//функция рисования шарика
+            public void DrawBall(Graphics dc)
+            {
+                    dc.DrawEllipse(color, x, y, w, h);
+                    
+                    
+                
+            }
+            //конструктор класса
+            public Ball(int xn, int yn, int wn, int hn, int dxn, int dyn, Pen colorPen, bool v)
+            {
+                vis = v;
+                color = colorPen;
+                x = xn; y = yn; w = wn; h = hn; dx = dxn; dy = dyn;//инициализируем
+                thr = new Thread(new ThreadStart(FnThr)); //создаем потоковый объект
+                live = true; //устанавливаем признак жизни
+                thr.Start(); //запускаем поток
+            }
+        }
+        Ball[] bl = new Ball[10];//массив пустых ссылок типа Ball
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int j = 0; j < bl.Length; j++)
+            {
+                if(bl[j].vis)
+                bl[j].DrawBall(e.Graphics);//рисуем
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < bl.Length; j++)
+            {
+                bl[j].live = false;// Уничтожаем потоки
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            for (int j = 0; j < bl.Length; j++)
+            {
+                bl[j].live = false;//уничтожаем потоки
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+            bl[i].thr.Suspend();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+            bl[i].thr.Resume();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox2.Text);
+            bl[i].color = Pens.White;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox2.Text);
+            bl[i].color = Pens.Magenta;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox3.Text);
+            bl[i].vis = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(textBox3.Text);
+            bl[i].vis = true;
+        }
+    }
+
+}
